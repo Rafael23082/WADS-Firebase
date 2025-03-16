ded1bc082a6d26cb2cdb5f67830dfc620a4ccb56
Rafael23082 <144778682+Rafael23082@users.noreply.github.com>
2025-03-16 20:53:47 +0700

    Delete src/To-Do-List directory

--- src/To-Do-List/components/Button.jsx
+++
@@ -1,9 +0,0 @@
-import { useNavigate } from "react-router-dom";
-
-export const Button = ({color, hover, message, navigateTo}) => {
-    const navigate = useNavigate();
-
-    return (
-        <button className={`px-6 py-3 rounded-xl m-5 mt-10 border-none ${color} text-white ${hover}`} onClick={() => {navigate(navigateTo)}}>{message}</button>
-    );
-}
